e176ea8cbf203cdf8bc08ffe027abe758697b4fc
quinnsweeeney <quinn.sweeney1@marist.edu>
2022-10-02 20:08:07 -0400

    slay the day away

--- Assignment 2/if_else_practice.py
+++ Assignment 2/if_else_practice.py
@@ -1,16 +1,19 @@
-foods = ["cum", "any form of potatoes", "idk waffles"]
+def main():
+    '''ask the user for their age. If the user is 25 or older, tell them they can buy alcohol, nicotine products, and they can rent a car
+        If they're 21 or older but younger than 25, tell them they can buy alcohol and nicotine products, but cannot rent a car
+        If they're 18 and older but younger than 21, tell them they can only buy nicotine products in some states
+        If they're less than 18, they can only purchase candy cigarettes and sody pops
+    '''
 
-print(foods[0])
+age = input("What's your age?")
+age = int(age)
+if age >= 25:
+    print("You may buy alcohol, nicotine products, and rent a car")
+elif age >= 21 and age < 25:
+    print("You may buy alcohol and nicotine products, but you cannot rent a car")
+elif age >= 18 and age < 21:
+    print("You can buy nicotine products in some states")
+else:
+    print("You may only purchase candy cigarettes and sody pops")
 
-foods[0] = "women"
-print(foods)
-
-foods.insert(3, "women")
-print(foods)
-
-foods.append("women")
-print(foods)
-
-foods.remove("any form of potatoes")
-foods.remove("idk waffles")
-print(foods)
+main()
