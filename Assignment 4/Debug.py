29792edc35a8d46bd8301b1b93e2c23023b6cb15
quinnsweeeney <quinn.sweeney1@marist.edu>
2022-12-12 12:58:23 -0500

    i dont like debug smh

--- Assignment 4/Debug.py
+++ Assignment 4/Debug.py
@@ -1,46 +1,50 @@
 
-def printHello():
+def printhello():
     print("Hello")
 
 
-def printName():
+def printname():
     print("Quinn")
-    
+
+
 def addition(x, y):
-    #add x and y together and return them
-    return(x + y)
+    # add x and y together and return them
+    return x + y
+
 
 def smaller(i, j):
-    #if i is smaller than j, return i
-    #if j is smaller than i, return j
-    #if they're even, return 0
+    # if i is smaller than j, return i
+    # if j is smaller than i, return j
+    # if they're even, return 0
     if i < j:
         return i
     elif j < i:
         return j
     else:
         return 0
+
+
 def main():
 
-    #call the printHello function here
-    printHello()
+    # call the printHello function here
+    printhello()
 
-    #call printName and give it the parameter of your name
-    printName()
-
+    # call printName and give it the parameter of your name
 
 
-    main()
+printname()
 
-    var1= 10
-    var2= 20
-    #What do we put in here to make it work?
-    print(addition())
+main()
+
+var1 = 10
+var2 = 20
+# What do we put in here to make it work?
+print(addition(var1, var2))
     
-    num1 = int(input("Enter number 1"))
-    num2 = int(input("Enter number 2"))
-    #what go here?
-    print(smaller())
+num1 = int(input("Enter number 1"))
+num2 = int(input("Enter number 2"))
+# what go here?
+print(smaller(num1, num2))
 
 
 
