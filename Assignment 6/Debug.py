11a834b5872f1a658e5a3e51c93b3dbfa72578c3
quinnsweeeney <quinn.sweeney1@marist.edu>
2022-12-12 14:03:40 -0500

    cake

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -12,6 +12,9 @@ class Employee:
 class Cake:
     #can you fill out the rest of this for me? im dumb
     #the cake needs to have the cake flavor and cake frosting stored
+    def __init__(self, flavor, frosting):
+        self.flavor = flavor
+        self.frosting = frosting
 
 class Cat:
     def __init__(self, name, age, fur_length):
@@ -33,35 +36,44 @@ class Car:
         self.color = color
         
     #create your own function! what do you want it to do?
+    def runOrNot(self):
+        if self.year >= 2006:
+            return("Not dead yet")
+        else:
+            return("rip those wheels shawty")
     
    
 def main():
     #fill this one out with a dog's name and age.. can be your dog, friend's dog, etc
-    newDog = Dog(? , ?)
+    dog1 = Dog("Baldwin", 3)
     print(dog1.name, dog1.age)
     
     #and what about a new employee
-    newEmployee =
+    newEmployee = Employee("Quinn Sweeney", 4272004 , "Food Critic")
     #how would we print out each of the variables from newEmployee?
-    print()
+    print(newEmployee.name, newEmployee.idNumber, newEmployee.department)
     
     #now create and print out a cake you make
-    
-    
+    cake1 = Cake("Red Velvet", "Cream Cheese")
+    print(cake1.flavor, cake1.frosting)
     
     #and now create another cake and print it out
-    
+    cake2 = Cake("Chocolate", "Buttercream")
+    print(cake2.flavor, cake2.frosting)
     
     
     #create a cat!
-    cat1 = Cat()
+    cat1 = Cat("Luna", 5, "long")
     #create another cat!
-    
+    cat2 = Cat("Loki", 5, "long")
     #What would we put here to print out the result of breedGuess for cat1?
-    print(cat1.?)
+    print(cat1.breedGuess())
     
     #create a car!
+    car1 = Car("Honda Civic", 2006, "White")
     
     #Now print out the function you made for car :)
+    print(car1.runOrNot())
+
 
 main()
